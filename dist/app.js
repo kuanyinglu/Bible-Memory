e4987f5d6c85bb368eb1d8f54be5fe68504f08dc
kuanyinglu <kuanyinglu@utexas.edu>
2019-07-21 22:52:45 -0500

    Express redirect

--- dist/app.js
+++ dist/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
+app.get(['/', '/verses', '/settings', '/practice'], function (req, res) {
     res.sendFile(__dirname + '/Index.html');
 });
+app.get(['/verses', '/settings', '/practice'], function (req, res) {
+    res.redirect('/');
+});
+
 app.use(express.static(__dirname)); 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
