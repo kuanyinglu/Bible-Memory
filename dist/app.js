12a65be35c1dbf22944e9b111d84a5c53abf2d26
kuanyinglu <kuanyinglu@utexas.edu>
2019-07-25 18:18:11 -0500

    fixed much

--- dist/app.js
+++ dist/app.js
@@ -3,28 +3,29 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
-const clientId = process.env.CLIENT_ID || "794214659031-93k2plhtt3mhinrfvcj825ohkvckk0v5.apps.googleusercontent.com";
+const clientId = process.env.CLIENT_ID;
+const domain = process.env.DOMAIN;
+const token = process.env.TOKEN;
 
 const {OAuth2Client} = require('google-auth-library');
 const client = new OAuth2Client(clientId);
 
-async function authenticate(idToken) {
-  if (typeof idToken === 'undefined' || typeof clientId === 'undefined') {
-      return null;
+const authenticate = async function authenticate(idToken) {
+  if (typeof idToken === 'undefined' || idToken.length === 0 || typeof clientId === 'undefined' || clientId.length === 0) {
+    return null;
+  } else {
+    const ticket = await client.verifyIdToken({
+        idToken: idToken,
+        audience: clientId,
+    });
+    const payload = ticket.getPayload();
+    if (typeof payload === 'undefined' || payload === null || payload['hd'] !== domain || payload['aud'] !== clientId)
+    {
+        return null;
+    } else {
+        return idToken;
+    }
   }
-  
-  const ticket = await client.verifyIdToken({
-      idToken: idToken,
-      audience: clientId,
-  });
-  const payload = ticket.getPayload();
-  console.log("Payload: ");
-  console.log(payload);
-  if (payload['hd'] !== 'utexas.edu' && payload['aud'] !== clientId)
-  {
-      return null;
-  }
-  return idToken;
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,14 +34,15 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    console.log("Cookies: ");
-    console.log(req.cookies);
-    var idToken = authenticate(req.cookies.idToken);
-    if (idToken !== null) {
-        res.render(__dirname + '/Index.ejs');
-    } else {
+    authenticate(req.cookies.idToken).then(function(idToken) {
+        if (typeof idToken !== 'undefined' && idToken !== null) {
+            res.render(__dirname + '/Index.ejs');
+        } else {
+            res.redirect('/login');
+        }
+    }).catch(function(){
         res.redirect('/login');
-    }
+    });
 });
 
 app.get(['/verses', '/settings', '/practice'], function (req, res) {
@@ -48,48 +50,68 @@ app.get(['/verses', '/settings', '/practice'], function (req, res) {
 });
 
 app.get(['/login',], function (req, res) {
-    var idToken = authenticate(req.cookies.idToken);
-    if (idToken === null) {
-        res.render(__dirname + '/Login.ejs', {clientId: clientId});
-    } else {
-        res.redirect('/');
-    }
+    authenticate(req.cookies.idToken).then(function(idToken) {
+        if (typeof idToken === 'undefined' || idToken === null) {
+            res.render(__dirname + '/Login.ejs', {clientId: clientId, domain: domain});
+        } else {
+            res.redirect('/');
+        }
+    }).catch(function(){
+        res.render(__dirname + '/Login.ejs', {clientId: clientId, domain: domain});
+    });
 });
 
 app.post(['/authenticate',], function (req, res) {
-    var idToken = authenticate(req.body.id);
-    if (idToken !== null) {
-        res.cookie("idToken", idToken, { maxAge: 3600000, secure: true, httpOnly: true });
-    } else {
+    authenticate(req.body.id).then(function(idToken) {
+        if (typeof idToken !== 'undefined' && idToken !== null) {
+            res.cookie("idToken", idToken, { maxAge: 3600000, secure: true, httpOnly: true });
+            res.redirect('/');
+        } else {
+            res.send(401, 'error');
+        }
+    }).catch(function(){
         res.redirect('/');
-    }
+    });
 });
 
 app.get('/token.js', function (req, res) {
-    var idToken = authenticate(req.cookies.idToken);
-    if (idToken !== null) {
-        res.sendFile(__dirname + '/token.js');
-    } else {
+    authenticate(req.cookies.idToken).then(function(idToken) {
+        if (typeof idToken !== 'undefined' && idToken !== null) {
+            if (typeof token !== 'undefined') {
+                res.send("var token = \"" + token + "\";");
+            } else {
+                res.sendFile(__dirname + '/token.js');
+            }
+        } else {
+            res.send(401, 'error');
+        }
+    }).catch(function(){
         res.send(401, 'error');
-    }
+    });
 });
 
 app.get('/verses.js', function (req, res) {
-    var idToken = authenticate(req.cookies.idToken);
-    if (idToken !== null) {
-        res.sendFile(__dirname + '/verses.js');
-    } else {
+    authenticate(req.cookies.idToken).then(function(idToken) {
+        if (typeof idToken !== 'undefined' && idToken !== null) {
+            res.sendFile(__dirname + '/verses.js');
+        } else {
+            res.send(401, 'error');
+        }
+    }).catch(function(){
         res.send(401, 'error');
-    }
+    });
 });
 
 app.get('/bundle.js', function (req, res) {
-    var idToken = authenticate(req.cookies.idToken);
-    if (idToken !== null) {
-        res.sendFile(__dirname + '/bundle.js');
-    } else {
+    authenticate(req.cookies.idToken).then(function(idToken) {
+        if (typeof idToken !== 'undefined' && idToken !== null) {
+            res.sendFile(__dirname + '/bundle.js');
+        } else {
+            res.send(401, 'error');
+        }
+    }).catch(function(){
         res.send(401, 'error');
-    }
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
