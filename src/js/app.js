0920dd057c8dc265ba24c3efb2af98a8f8dbfac7
kuanyinglu <kuanyinglu@utexas.edu>
2019-07-24 21:15:36 -0500

    test

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const authenticate = require('./authentication.js');
 const cookieParser = require('cookie-parser')
 const app = express();
 const port = process.env.PORT || 3000;
 const clientId = process.env.CLIENT_ID;
+
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+const authenticate = idToken => {
+  if (typeof idToken !== 'undefined' && typeof clientId !== 'undefined') {
+      return null;
+  }
+  
+  const ticket = await client.verifyIdToken({
+      idToken: idToken,
+      audience: CLIENT_ID,
+  });
+  const payload = ticket.getPayload();
+  if (payload['hd'] !== 'utexas.edu' && payload['aud'] !== clientId)
+  {
+      return null;
+  }
+  return idToken;
+};
 
 app.use(cookieParser());
 
