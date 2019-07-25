8b2f129ee8d655ca221203f0f494af367f539b66
kuanyinglu <kuanyinglu@utexas.edu>
2019-07-24 21:25:08 -0500

    fixed build

--- build-prod.js
+++ build-prod.js
@@ -4,4 +4,3 @@ shell.cp('./src/html/Index.ejs', './dist/Index.ejs');
 shell.cp('./src/html/Login.ejs', './dist/Login.ejs');
 shell.cp('./src/js/app.js', './dist/app.js');
 shell.exec('npx webpack -p  --config webpack.production.config.js');
-shell.exec('node ./dist/app.js');
