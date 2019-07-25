f77bac0d030e4140a05715be9d774d0ecacfc927
kuanyinglu <kuanyinglu@utexas.edu>
2019-07-24 21:11:31 -0500

    fixed authentication

--- src/js/authentication.js
+++ src/js/authentication.js
@@ -2,19 +2,21 @@ const {OAuth2Client} = require('google-auth-library');
 const client = new OAuth2Client(CLIENT_ID);
 const clientId = process.env.CLIENT_ID;
 
-exports.authenticate = idToken => {
-    if (typeof idToken !== 'undefined' && typeof clientId !== 'undefined') {
-        return null;
-    }
-    
-    const ticket = await client.verifyIdToken({
-        idToken: idToken,
-        audience: CLIENT_ID,
-    });
-    const payload = ticket.getPayload();
-    if (payload['hd'] !== 'utexas.edu' && payload['aud'] !== clientId)
-    {
-        return null;
-    }
-    return idToken;
-  };
+const authenticate = idToken => {
+  if (typeof idToken !== 'undefined' && typeof clientId !== 'undefined') {
+      return null;
+  }
+  
+  const ticket = await client.verifyIdToken({
+      idToken: idToken,
+      audience: CLIENT_ID,
+  });
+  const payload = ticket.getPayload();
+  if (payload['hd'] !== 'utexas.edu' && payload['aud'] !== clientId)
+  {
+      return null;
+  }
+  return idToken;
+};
+
+module.exports = authenticate;
