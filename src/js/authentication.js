b460004f9fb05ef78a7176a47651c3d863f63727
kuanyinglu <kuanyinglu@utexas.edu>
2019-09-12 07:47:15 -0500

    Test

--- src/js/authentication.js
+++ src/js/authentication.js
@@ -1,5 +1,7 @@
 module.exports = {
   authenticate: async function(idToken, client, clientId) {
+    console.log("ClientId length: " + clientId.length);
+    console.log("token length: " + idToken.length);
     if (typeof idToken === 'undefined' || idToken.length === 0 || typeof clientId === 'undefined' || clientId.length === 0) {
       return null;
     } else {
@@ -10,6 +12,7 @@ module.exports = {
       const payload = ticket.getPayload();
       if (typeof payload === 'undefined' || payload === null || payload['hd'] !== domain || payload['aud'] !== clientId)
       {
+        console.log("not verified");
         return null;
       } else {
         return idToken;
